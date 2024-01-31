0aae5026ca4b0617b484e7b73eb83dbf55a1509d
andrrescely <134743627+andrrescely@users.noreply.github.com>
2024-01-31 18:26:27 -0500

    arreglo de colores e iconos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth/auth.component';
 import { PagesComponent } from './pages/pages.component';
+import { FincasEditComponent } from './fincas/fincas-edit/fincas-edit.component';
 
 const routes: Routes = [
-  {path:'', redirectTo: '/login', pathMatch:'full'},
-  {path:'login', component: AuthComponent},
-  {path: 'dashboard', component:PagesComponent,
-children:[
-  {path: 'fincas',
-  loadChildren:()=> import('./fincas/fincas.module').then(m =>m.FincasModule)
-}
-]}
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: AuthComponent },
+  { path: 'nosotros', component: FincasEditComponent },
+  {
+    path: 'dashboard',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'fincas',
+        loadChildren: () =>
+          import('./fincas/fincas.module').then((m) => m.FincasModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
