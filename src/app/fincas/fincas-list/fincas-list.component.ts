8ec8ef3b3484efb88d7cf85a0defa597d0f80d81
andrrescely <134743627+andrrescely@users.noreply.github.com>
2024-02-14 13:47:20 -0500

    estetica

--- src/app/fincas/fincas-list/fincas-list.component.ts
+++ src/app/fincas/fincas-list/fincas-list.component.ts
@@ -20,6 +20,7 @@ export class FincasListComponent {
       claseNombre: 'clase-elegante',
       claseDescripcion: 'clase-normal',
       capacidad: 'Capacidad :  20 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Hacienda El Paraíso',
@@ -27,6 +28,7 @@ export class FincasListComponent {
         'https://cdn0.matrimonio.com.co/vendor/0025/3_2/960/jpg/casa9_10_120025.jpeg',
       descripcion: 'Ubicación: Armenia, Quindío',
       capacidad: 'Capacidad :  50 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca Los Pájaros',
@@ -34,6 +36,7 @@ export class FincasListComponent {
         'https://finkeros_listings.s3.amazonaws.com/xxl_515_1369325150.5083_b3505f80e0.jpg',
       descripcion: 'Ubicación: Santa Marta, Magdalena',
       capacidad: 'Capacidad :  30 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Rancho del Sol',
@@ -41,6 +44,7 @@ export class FincasListComponent {
         'https://thumbs.apts247.info/resize/?img=https://media.apts247.info/a8/a8ce8bf808ac447181f6fca02cbc45db/galleries/community/amenities/0U6A0258.jpg&w=710',
       descripcion: 'Ubicación: Villavicencio, Meta',
       capacidad: 'Capacidad :  60 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Hotel campestre Monterrey',
@@ -48,6 +52,7 @@ export class FincasListComponent {
         'https://www.hotelcampestremonterrey.com/media/27288/hotel-monterrey-campestre-san-gil-fachada.jpg?width=725&height=507&mode=crop',
       descripcion: 'Ubicación: San Gil, Santander',
       capacidad: 'Capacidad :  100 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca La Vista Hermosa',
@@ -55,6 +60,7 @@ export class FincasListComponent {
         'https://www.bienesonline.com/colombia/photos/se-alquila-hermosa-finca-en-caluce-palmira-14644483170.jpg',
       descripcion: 'Ubicación: Cali, Valle del Cauca',
       capacidad: 'Capacidad :  15 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Hacienda Calibio capilla ph',
@@ -62,12 +68,14 @@ export class FincasListComponent {
         'https://i.pinimg.com/originals/a5/55/61/a55561ffbff6656ebd34e047ce76bb9f.jpg',
       descripcion: 'Ubicación: Popayán, Cauca',
       capacidad: 'Capacidad :  18 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Rancho Verde',
       imagen: 'https://villageverde.com.br/01/bg8.jpg',
       descripcion: 'Ubicación: Medellín, Antioquia',
       capacidad: 'Capacidad :  20 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca El Oasis de Icor',
@@ -75,6 +83,7 @@ export class FincasListComponent {
         'https://cdn0.bodas.net/vendor/43653/3_2/1280/jpg/altar-piscina_1_143653-157546287512764.jpeg',
       descripcion: 'Ubicación: Barranquilla, Atlántico',
       capacidad: 'Capacidad :  20 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Hacienda La Mariposa',
@@ -82,6 +91,7 @@ export class FincasListComponent {
         'https://pix10.agoda.net/hotelImages/176/176329/176329_13020700050010317043.jpg?s=1024x768',
       descripcion: 'Ubicacion: Bogota',
       capacidad: 'Capacidad :  20 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca 3',
@@ -89,6 +99,7 @@ export class FincasListComponent {
         'https://casasrusticas.org/wp-content/uploads/2021/07/casas-de-fincas-en-la-sierra-11.jpg',
       descripcion: 'Descripción de la Finca 11',
       capacidad: 'Capacidad :  20 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca 3',
@@ -96,6 +107,7 @@ export class FincasListComponent {
         'https://www.villasfincas.com/wp-content/uploads/2019/06/dsc_5815-1.jpg',
       descripcion: 'Descripción de la Finca 12',
       capacidad: 'Capacidad :  20 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca 3',
@@ -103,36 +115,42 @@ export class FincasListComponent {
         'https://cdn.pixabay.com/photo/2015/03/21/16/31/farm-683866_960_720.jpg',
       descripcion: 'Descripción de la Finca 12',
       capacidad: 'Capacidad :  20 personas',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca 3',
       imagen:
         'https://casasdecampo.blog/wp-content/uploads/2022/04/fincas-8.jpg',
       descripcion: 'Descripción de la Finca 14',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca 3',
       imagen:
         'https://casasrusticas.org/wp-content/uploads/2021/07/Bellas-casas-de-fincas-02.jpg',
       descripcion: 'Descripción de la Finca 15',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca 3',
       imagen:
         'https://immobilien.porta-mallorquina.de/115135/finca-campos-toller-poolbereich.jpg',
       descripcion: 'Descripción de la Finca 16',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca 3',
       imagen:
         'https://casasrusticas.org/wp-content/uploads/2021/07/casas-de-fincas-en-la-sierra-14.jpg',
       descripcion: 'Descripción de la Finca 17',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
     {
       nombre: 'Finca 3',
       imagen:
         'https://i.pinimg.com/originals/21/cc/7c/21cc7cde5177005ae3a09a901291084b.jpg',
       descripcion: 'Descripción de la Finca 17',
+      precio: 'Precio por noche : $70.000.00 cop',
     },
   ];
 
