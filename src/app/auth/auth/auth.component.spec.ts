df08e359b2e64f4135cc376ad20a1c94028c9bd6
andrrescely <134743627+andrrescely@users.noreply.github.com>
2024-02-16 16:41:26 -0500

    ejemplo de testing

--- src/app/auth/auth/auth.component.spec.ts
+++ src/app/auth/auth/auth.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthComponent } from './auth.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
 
+  //verifica que el componente se crea correctamente
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthComponent ]
-    })
-    .compileComponents();
+      declarations: [AuthComponent],
+      imports: [ReactiveFormsModule], // Agrega ReactiveFormsModule a los imports para utilizar formularios reactivos
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AuthComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,10 @@ describe('AuthComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should initialize the form correctly', () => {
+    expect(component.formLogin).toBeDefined(); // Verifica que el formulario se haya inicializado
+    expect(component.formLogin.get('email')).toBeDefined(); // Verifica que el campo de correo electrónico esté definido
+    expect(component.formLogin.get('password')).toBeDefined(); // Verifica que el campo de contraseña esté definido
   });
 });
